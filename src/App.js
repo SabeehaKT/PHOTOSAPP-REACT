d3bbb610c3ecfd82b65deb70e57532cfd2642d53
SabeehaKT <kampalasabeeha@gmail.com>
2024-06-08 01:33:36 +0530

    PhotosApp with React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AddPhotos from './Components/AddPhotos';
+import SearchPhotos from './Components/SearchPhotos';
+import DeletePhotos from './Components/DeletePhotos';
+import ViewAll from './Components/ViewAll';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<AddPhotos/>}/>
+      <Route path="/search" element={<SearchPhotos/>}/>
+      <Route path="/delete" element={<DeletePhotos/>}/>
+      <Route path="/view" element={<ViewAll/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/AddPhotos.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const AddPhotos = () => {
+  return (
+    <div>
+        <NavBar/>
+        <div className="container">
+            <div className="row">
+                <div className="ol col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">UserName</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Email ID</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Photo ID</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Photo Name</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Size</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Upload</label>
+                            <input type="file" name="" id="" className="form-control" />
+                        </div>
+                        <br/>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <button className="btn btn-success">Add</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+  )
+}
+
+export default AddPhotos
